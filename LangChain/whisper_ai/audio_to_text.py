8e1fafd6e67593e91c57e03968d05ffa321aadde
DaniyarTheGreat <daniyarsaifuding@gmail.com>
2023-10-15 08:41:09 -0400

    Added new requirements and a function that generates an aduio file

--- LangChain/whisper_ai/audio_to_text.py
+++ LangChain/whisper_ai/audio_to_text.py
@@ -1,18 +1,42 @@
 import openai
 from langchain.chat_models import ChatOpenAI
+import sounddevice as sd
+import numpy as np
+import wavio
+
 
 OPENAI_API_KEY = 'API KEY'
 
-audio_file = open("AUDIO FILE PATH","rb")
+# function that accesses microphone and records audio; duration is the time length of the audio file, defult is 10 seconds
+def record_audio(filename="output.wav", duration=10, samplerate=44100):
+    # Capture the audio
+    print(f"Recording for {duration} seconds...")
 
-openai.api_key = OPENAI_API_KEY
+    # if voice channel gives error, try print(sd.query_devices()) in main
+    # It should give you a list of all available audio devices and their properties, including the maximum input channels they support
+    # Then specify the device you want or try setting channels to 2
+    audio_data = sd.rec(int(samplerate * duration), samplerate=samplerate, channels=1, dtype='float32', device=0)
+    sd.wait()
 
-transcript = openai.Audio.translate("whisper-1",audio_file)
-user_transcript = transcript["text"]
+    # Save the audio to a file
+    wavio.write(filename, audio_data, samplerate, sampwidth=2)
+    print(f"Recording saved to {filename}")
+    return filename
 
-chat_model = ChatOpenAI(
-    temperature = 1,
-    openai_api_key = OPENAI_API_KEY
-)
+if __name__ == "__main__":
+    filepath = record_audio()
+    print(f"File saved at: {filepath}")
+ 
+    audio_file = open(filepath,"rb")
 
-print(chat_model.predict(user_transcript))
+    openai.api_key = OPENAI_API_KEY
+
+    transcript = openai.Audio.translate("whisper-1",audio_file)
+    user_transcript = transcript["text"]
+
+    chat_model = ChatOpenAI(
+        temperature = 1,
+        openai_api_key = OPENAI_API_KEY
+    )
+
+    print(chat_model.predict(user_transcript))
