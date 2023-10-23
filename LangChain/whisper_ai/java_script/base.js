0ee1252f24617ce00b1cb7a056bc358f8e896d23
DaniyarTheGreat <daniyarsaifuding@gmail.com>
2023-10-23 16:47:08 -0400

    java script audio record function

---
+++ LangChain/whisper_ai/java_script/base.js
@@ -0,0 +1,11 @@
+const recorder = require('node-record-lpcm16')
+const fs = require('fs')
+ 
+const file = fs.createWriteStream('test.wav', { encoding: 'binary' })
+ 
+const recording = recorder.record()
+recording.stream().pipe(file)
+
+setTimeout(() => {
+    recording.stop()
+  }, 5000)
