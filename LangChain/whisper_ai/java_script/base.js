a618568bd87067a13d3f550fb28196c32b34aef5
DaniyarTheGreat <daniyarsaifuding@gmail.com>
2023-10-24 10:23:13 -0400

    uploaded code to convert audio to text on base.js

--- LangChain/whisper_ai/java_script/base.js
+++ LangChain/whisper_ai/java_script/base.js
@@ -1,11 +1,26 @@
-const recorder = require('node-record-lpcm16')
-const fs = require('fs')
+const recorder = require("node-record-lpcm16")
+const OpenAI = require("openai")
+const fs = require("fs")
  
-const file = fs.createWriteStream('test.wav', { encoding: 'binary' })
- 
+const file = fs.createWriteStream("test.wav", { encoding: "binary" })
+
 const recording = recorder.record()
 recording.stream().pipe(file)
 
 setTimeout(() => {
     recording.stop()
+    audioConvert();
   }, 5000) // 5 seconds
+
+const openai = new OpenAI({
+  apiKey: "API KEY"
+})
+
+// Converting audio to text, prints the transcript
+const audioConvert=async()=>{
+  const transcription=await openai.audio.transcriptions.create({
+    file:fs.createReadStream("test.wav"),
+    model:"whisper-1"
+  })
+  console.log(transcription)
+}
