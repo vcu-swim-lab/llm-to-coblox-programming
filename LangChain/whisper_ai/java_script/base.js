9effec75ae8d98f6ca38559af226f1090864364e
DaniyarTheGreat <daniyarsaifuding@gmail.com>
2023-10-24 10:34:48 -0400

    Error handling/prevents audio file from stacking up

--- LangChain/whisper_ai/java_script/base.js
+++ LangChain/whisper_ai/java_script/base.js
@@ -1,26 +1,37 @@
-const recorder = require("node-record-lpcm16")
-const OpenAI = require("openai")
-const fs = require("fs")
- 
-const file = fs.createWriteStream("test.wav", { encoding: "binary" })
+const recorder = require("node-record-lpcm16");
+const OpenAI = require("openai");
+const fs = require("fs");
 
-const recording = recorder.record()
-recording.stream().pipe(file)
+const file = fs.createWriteStream("test.wav", { encoding: "binary" });
 
-setTimeout(() => {
-    recording.stop()
-    audioConvert();
-  }, 5000) // 5 seconds
+const recording = recorder.record();
+recording.stream().pipe(file);
+
+setTimeout(async () => {
+    recording.stop();
+
+    // Wait for a bit to ensure the file write is completed
+    setTimeout(async () => {
+        try {
+            await audioConvert();
+            
+            // Delete the temporary WAV file after transcription
+            fs.unlinkSync("test.wav");
+        } catch (error) {
+            console.error("Error during audio conversion:", error);
+        }
+    }, 1000); // Wait 1 second
+
+}, 5000); // 5 seconds
 
 const openai = new OpenAI({
-  apiKey: "API KEY"
-})
+  apiKey: "API KEY" 
+});
 
-// Converting audio to text, prints the transcript
-const audioConvert=async()=>{
-  const transcription=await openai.audio.transcriptions.create({
-    file:fs.createReadStream("test.wav"),
-    model:"whisper-1"
-  })
-  console.log(transcription)
+const audioConvert = async () => {
+  const transcription = await openai.audio.transcriptions.create({
+    file: fs.createReadStream("test.wav"),
+    model: "whisper-1"
+  });
+  console.log(transcription);
 }
